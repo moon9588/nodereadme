d79218ebc4f4444daf59c32fa13698740f645be2
moon9588 <moon9588@yahoo.com>
2022-02-01 21:35:59 -0500

    worked on index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const generateMarkdown = require('./util/generateMarkdown');
+ 
+const licenseChoices = [
+    "Apache 2.0",
+    "Microsoft Public License",
+    "None"
+];
